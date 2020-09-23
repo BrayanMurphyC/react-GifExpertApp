bc6d1ad12d5c13ae73115679c2c4c7523ef7a805
BrayanMurphyC <brayanmurphyc@gmail.com>
2020-09-23 18:24:55 -0500

    priemer

---
+++ docs/precache-manifest.25485a9749f1f3e3678a2a1164c8d729.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642f3325b05c0b2e93980e9c25ed598b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "857f59024b43aca700bf",
+    "url": "/static/css/main.9fdefa38.chunk.css"
+  },
+  {
+    "revision": "0de6988e9e8c418fd8f9",
+    "url": "/static/js/2.e4969178.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e4969178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "857f59024b43aca700bf",
+    "url": "/static/js/main.fb7dc604.chunk.js"
+  },
+  {
+    "revision": "20dc2048170b8f653adb",
+    "url": "/static/js/runtime-main.93240507.js"
+  }
+]);
