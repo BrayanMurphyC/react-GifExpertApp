3230509a9a9f1fcf1a51b13dc7d1a113b286c7ec
BrayanMurphyC <brayanmurphyc@gmail.com>
2020-09-23 16:37:42 -0500

    mostrar titulos listados

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ( { category } ) => { //recibimos los props entre llaves desde el padre
 
+    // const [count, setCount] = useState(0)
+    
+    // useEffect es para que se ejecuta una sola vez el componente
+    useEffect(() => {
+        getGifs();       
+    }, [])
+    const [images, setImages] = useState([])
 
+    // Obtener las imagenes con fech
     const getGifs = async()  =>{
         const url = 'https://api.giphy.com/v1/gifs/search?api_key=fvds0FidlulyF0AHSYe108wmbeVsseZQ&q=goku&limit=10&offset=0&rating=g&lang=en';
         const resp = await fetch( url );
@@ -17,13 +26,23 @@ export const GifGrid = ( { category } ) => { //recibimos los props entre llaves 
         })
 
         console.log(gifs);
+        setImages(gifs);
     }
 
-    getGifs();
+   
 
     return (
         <div>
             <h3>{ category } </h3>
+            
+                { 
+                    images.map( img => (
+                       <GifGridItem 
+                       key={img.id}
+                       img={ img }/>              
+                    ))
+                }
+            
         </div>
     )
 }
